c15f1626cc3d11083cc6ba525072351cc06e4e00
Katsumi INOUE <thekinoue@hotmail.com>
2017-08-21 20:27:39 +0900

    Added help. Fixed duration bug.

--- GIFcaption.sh
+++ GIFcaption.sh
@@ -1,40 +1,39 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #export LC_ALL=ja_JP
 fontsize=48
 bgcolor=black
-duration=300
+captionduration=300
 
-while getopts "g:c:b:s:d:" option; do
+usage() {
+  echo -e "Usage:\n$0 -g (gif filename) -c (caption string) -b (background color) \\ \n  -s (font size) -d (seconds to show caption image)\n"
+}
+
+while getopts "g:c:b:s:d:h" option
+do
   case $option in
-    g)
-        filename=$OPTARG
-    ;;
-    c)
-        caption=$OPTARG
-    ;;
-    b)
-        bgcolor=$OPTARG
-    ;;
-    s)
-        fontsize=$OPTARG
-    ;;
-    d)
-        captionduration=$((OPTARG*100))
-    ;;
+    g) filename=$OPTARG ;;
+    c) caption=$OPTARG ;;
+    b) bgcolor=$OPTARG ;;
+    s) fontsize=$OPTARG ;;
+    d) captionduration=$((OPTARG*100)) ;;
+    h) usage; exit 1;;
   esac
 done
 
+[ -z "$filename" ] && usage && exit 1
+
 size=$(file "$filename"|grep  -Eo '[0-9]+ x .*'|tr -d ' ')
 duration=$(magick identify -format "%T\n" "$filename"| awk '{t+=$0};END{print t}')
-#[ ${#filename} -gt 28 ] && fontsize=32
 
 fnbase=$(echo "$filename" | sed 's/\.[gG][iI][fF]$//')
 if [ -z "$caption" ]; then
     caption=$fnbase
 fi
 
+# Create a image file using 2 strings.
 magick convert -size $size -background $bgcolor -font c:/Windows/Fonts/meiryob.ttc \
   -pointsize $fontsize -fill white -gravity center \
   caption:"${caption}\n$(($duration/100))秒" "${fnbase}_fndur.gif"
-#  label:"${filename}\n$(($duration/100))秒" "${filename}_fndur.gif"
+#  label:"${caption}\n$(($duration/100))秒" "${fnbase}_fndur.gif"
+
 magick convert "$filename" \( "${fnbase}_fndur.gif" -set delay $captionduration \) "${fnbase}_appended.gif"
